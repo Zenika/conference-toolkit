417e2825f649d63bc502b22726ec0c32fae24248
Aurélien Loyer <aur.loy@gmail.com>
2019-02-20 22:10:46 -0500

    feat: add logo widget 🖌

---
+++ src/components/Logo.vue
@@ -0,0 +1,26 @@
+<template>
+    <img class="logo" src="/img/znk2.png" alt="">
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+  data: function() {
+      return {}
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.logo {
+    height: 20vh;
+    overflow: hidden;
+    position: absolute;
+    // left: 50px;
+    // bottom: 20px;
+    top: 35px;
+    left: 220px;
+    z-index: 1000;
+    opacity: 0.6;
+}
+</style>
