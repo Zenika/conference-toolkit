b9e2aebb1f00e15907d98325a5a303307ce7a24c
Aurélien Loyer <aur.loy@gmail.com>
2019-02-21 22:11:42 -0500

    feat: add ltwitter widget 🐦

---
+++ src/components/Twitter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="twitter">
+      <a class="twitter-timeline" data-lang="fr" data-theme="dark" data-link-color="#E81C4F" :href="`https://twitter.com/${twittername}?ref_src=twsrc%5Etfw`">Tweets by ZenikaMontreal</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Twitter',
+  data: function() {
+      return {
+          // twittername: 'ZenikaMontreal',
+          twittername: 'AurelienLoyer'
+      }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.twitter {
+    width: 30vw;
+    height: calc(100vh - 150px);
+    overflow: hidden;
+    position: absolute;
+    right: 200px;
+    top: 20px;
+}
+</style>
