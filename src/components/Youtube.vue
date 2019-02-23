937266c3cdb0fbc7559cdaa7ec02221a05233fdd
Aurélien Loyer <aur.loy@gmail.com>
2019-02-22 22:12:39 -0500

    feat: add youtube widget 🍿

---
+++ src/components/Youtube.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="youtube">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/b_Gh5YIzs9o?&autoplay=1&mute=1" frameborder="0" allowfullscreen></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Youtube',
+  data: function() {
+      return {}
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.youtube {
+    width: 30vw;
+    height: calc(100vh - 150px);
+    overflow: hidden;
+    position: absolute;
+    right: 200px;
+    top: 20px;
+    iframe {
+      width: 100%;
+    }
+}
+</style>
