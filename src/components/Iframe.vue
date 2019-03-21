4b6f96c168a2fafc45beb361eb6296e40dd4750d
Aurélien Loyer <aur.loy@gmail.com>
2019-03-21 10:58:00 -0400

    feat: add Iframe slide #11 🎡

---
+++ src/components/Iframe.vue
@@ -0,0 +1,32 @@
+<template>
+    <iframe :src="src"></iframe>
+</template>
+
+<script>
+export default {
+  name: 'Iframe',
+  props: [
+      'src',
+      'left',
+      'right',
+      'top',
+      'bottom',
+  ],
+  data: function() {
+      return {}
+  },
+}
+</script>
+
+<style scoped lang="scss">
+iframe {
+    width: 30vw;
+    height: calc(100vh - 150px);
+    overflow: hidden;
+    position: absolute;
+    right: 200px;
+    top: 40px;
+    background: #0c1f2c;
+    border-radius: 10px;
+}
+</style>
